7ce0817ae02d846ac600df31178ac445b0a98a26
khoirularyan <khoirularyan123@gmail.com>
2025-10-01 18:09:26 +0700

    fungsi checklist isComplete, delete

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,61 +1,61 @@
-import React from "react";
-import todoitems from "./todoitems";
-import { useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import Todoitems from "./todoitems";
 
 export const Todo = () => {
-
-  const [Todolist, setTodolist] = React.useState([]);
+  const [todolist, setTodolist] = React.useState([]);
 
   const inputRef = useRef();
-  
+
   //fungsi add
   const add = () => {
     const inputText = inputRef.current.value.trim();
-    
-    if (inputText === ""){
+
+    if (inputText === "") {
       return null;
     }
-    
+
     //buat object baru
-    const newTodo = { 
+    const newTodo = {
       id: Date.now(),
-      Text: inputText,
+      text: inputText,
       completed: false,
-    }
+    };
     //pakai prev biar dapet value terbaru (previous)
     setTodolist((prev) => [...prev, newTodo]);
     inputRef.current.value = "";
-  }
+  };
 
   //fungsi delete
   const deleteTodo = (id) => {
-    setTodolist((prevTodo) =>{
+    setTodolist((prevTodo) => {
       return prevTodo.filter((todo) => todo.id !== id);
     });
   };
- 
+
   //fungsi complete
-  const completeTodo = (id) => {
+  const toggle = (id) => {
     setTodolist((prevTodo) => {
       return prevTodo.map((todo) => {
         if (todo.id === id) {
-          return { ...todo, completed: !todo.completed };
+          return { ...todo, isComplete: !todo.isComplete };
         }
         return todo;
       });
     });
   };
 
+  useEffect(() => {
+    console.log(todolist);
+  }, [todolist]);
+
   return (
     // frame
     <div className="bg-gray-800 place-self-center rounded-4xl flex flex-col p-2 w-11/12 max-w-md min-h-[550px]">
-      
       {/* header */}
       <div className="flex items-center place-self-center gap-2 ">
-      <p className="text-white font-semibold text-center ">To-Do List App</p>
+        <p className="text-white font-semibold text-center ">To-Do List App</p>
         <img
-            className="w-10 h-10"
+          className="w-10 h-10"
           src="./src/assets/checklist.png"
           alt="Logo"
         />
@@ -63,17 +63,33 @@ export const Todo = () => {
 
       {/* input button */}
       <div className="flex justify-between items-center gap-2 my-4 pl-5 pr-2">
-        <input ref={inputRef} className="flex-1 text-white border bg-slate-800 border-gray-500 pl-4 p-0.5 rounded-full" placeholder="Add a new task..." />
-        <button onClick={add} className="bg-gray-600 font-semibold text-white rounded-full ml-2 px-3 py-1 hover:bg-gray-700">Add +</button>
+        <input
+          ref={inputRef}
+          className="flex-1 text-white border bg-slate-800 border-gray-500 pl-4 p-0.5 rounded-full"
+          placeholder="Add a new task..."
+        />
+        <button
+          onClick={add}
+          className="bg-gray-600 font-semibold text-white rounded-full ml-2 px-3 py-1 hover:bg-gray-700"
+        >
+          Add +
+        </button>
       </div>
-    
-      <div> 
-      {Todolist.map((item, index) => {
-        return <Todoitems key={index} Text={item.Text} id={item.id} isComplete={item.isComplete} deleteTodo={deleteTodo} />
-      })}
 
-        {todoitems({ Text: "Learn React" })}
+      <div>
+        {todolist.map((item, index) => {
+          return (
+            <Todoitems
+              key={index}
+              Text={item.text}
+              id={item.id}
+              isComplete={item.completed}
+              deleteTodo={deleteTodo}
+              toggle={toggle}
+            />
+          );
+        })}
       </div>
     </div>
   );
-}
+};
--- src/components/Todoitems.jsx
+++ src/components/Todoitems.jsx
@@ -1,16 +1,33 @@
 import React from "react";
 
-const Todoitems = ({ Text, id, isComplete, deleteTodo}) => {
-
+const todoitems = ({ Text, id, isComplete, deleteTodo, toggle }) => {
   return (
     <div className="flex items-center justify-between p-2 mr-5 ml-4">
-      <div className="flex gap-4 items-center">
-      <img className="w-4" src="./src/assets/check-box.png" alt="" />
+      <div
+        onClick={() => {
+          toggle(id);
+        }}
+        className="flex gap-4 items-center cursor-pointer"
+      >
+        <img
+          className="w-4"
+          src={
+            isComplete
+              ? "./src/assets/check-box-checked.png"
+              : "./src/assets/check-box.png"
+          }
+          alt=""
+        />
         <p className="text-white">{Text}</p>
       </div>
 
-      <img onClick={() =>deleteTodo(id)} src="./src/assets/trash.png" alt="" className="w-4 h-4 ml-auto cursor-pointer " />
+      <img
+        onClick={() => deleteTodo(id)}
+        src="./src/assets/trash.png"
+        alt=""
+        className="w-4 h-4 ml-auto cursor-pointer "
+      />
     </div>
   );
-}
-export default Todoitems;
+};
+export default todoitems;
